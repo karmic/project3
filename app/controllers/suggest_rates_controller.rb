d9c3ff5fe50fa19982ef725fdbd37957fd02aa2a
karmic <buted@hawaii.edu>
2011-04-22 18:34:33 -1000

    rmv things from proj2

--- app/controllers/suggest_rates_controller.rb
+++ app/controllers/suggest_rates_controller.rb
@@ -6,16 +6,26 @@ class SuggestRatesController < ApplicationController
 	def chart
 		require 'gchart'
 
-		great = SuggestRate.count(:all,:conditions => ["rate=?", "great"])
-    neutral = SuggestRate.count(:all,:conditions => ["rate=?", "neutral"])
-    bad = SuggestRate.count(:all,:conditions => ["rate=?", "bad"])
-		@chart_suggest1 = Gchart.bar(:title => "Catherine's Suggestion 1",:title_size => 20, :data => [great, neutral, 
-		bad],:bar_width_and_spacing => 75,:bar_colors => 'E8AAF0', :labels => ["great: " + great.to_s, "neutral: " + neutral.to_s, "bad: " + bad.to_s])
-
 		great = SuggestRate.count(:all,:conditions => ["rate=?", "great1"])
     neutral = SuggestRate.count(:all,:conditions => ["rate=?", "neutral1"])
     bad = SuggestRate.count(:all,:conditions => ["rate=?", "bad1"])
-		@chart_suggest2 = Gchart.bar(:title => "Catherine's Suggestion 2",:title_size => 20, :data => [great, neutral, bad],:bar_width_and_spacing => 75,:bar_colors => 'E8AAF0', :labels => ["great: " + great.to_s, "neutral: " + neutral.to_s, "bad: " + bad.to_s])
+		@result_suggest1 = Gchart.bar(:title => "Suggestion 1",:title_size => 20, :data => [great, neutral, 
+		bad],:bar_width_and_spacing => 75,:bar_colors => 'E8AAF0', :labels => ["great: " + great.to_s, "neutral: " + 
+		neutral.to_s, "bad: " + bad.to_s])
+
+		great = SuggestRate.count(:all,:conditions => ["rate=?", "great2"])
+    neutral = SuggestRate.count(:all,:conditions => ["rate=?", "neutral2"])
+    bad = SuggestRate.count(:all,:conditions => ["rate=?", "bad2"])
+		@result_suggest2 = Gchart.bar(:title => "Suggestion 2",:title_size => 20, :data => [great, neutral, 
+		bad],:bar_width_and_spacing => 75,:bar_colors => 'E8AAF0', :labels => ["great: " + great.to_s, "neutral: " + 
+		neutral.to_s, "bad: " + bad.to_s])
+
+		great = SuggestRate.count(:all,:conditions => ["rate=?", "great3"])
+    neutral = SuggestRate.count(:all,:conditions => ["rate=?", "neutral3"])
+    bad = SuggestRate.count(:all,:conditions => ["rate=?", "bad3"])
+		@result_suggest3 = Gchart.bar(:title => "Suggestion 3",:title_size => 20, :data => [great, neutral, 
+		bad],:bar_width_and_spacing => 75,:bar_colors => 'E8AAF0', :labels => ["great: " + great.to_s, "neutral: " + 
+		neutral.to_s, "bad: " + bad.to_s])
 	
 		respond_to do |format|
 			format.html     	
@@ -29,13 +39,16 @@ class SuggestRatesController < ApplicationController
   end
 
 	def create
-		suggestion = params[:suggest_rate][:rate1]
-		suggestion1 = params[:suggest_rate][:rate2]
+		suggestion1 = params[:suggest_rate][:rate1]
+		suggestion2 = params[:suggest_rate][:rate2]
+		suggestion3 = params[:suggest_rate][:rate3]
 		conn = ActiveRecord::Base.connection
-		sql1 = "insert into rates(suggest_id,rate) values ('1','" + suggestion + 		"')"
-		sql2 = "insert into rates(suggest_id,rate) values ('2','" + suggestion1 + 		"')"
+		sql1 = "insert into rates(suggest_id,rate) values ('1','" + suggestion1 + 		"')"
+		sql2 = "insert into rates(suggest_id,rate) values ('2','" + suggestion2 + 		"')"
+		sql3 = "insert into rates(suggest_id,rate) values ('3','" + suggestion3 + 		"')"
 		conn.insert(sql1)
 		conn.insert(sql2)
+		conn.insert(sql3)
 		redirect_to :controller => "pages", :action => "continue"
 	end
 
--- tablesViews.sql
+++ tablesViews.sql
@@ -3,8 +3,8 @@
 -- create sequences
 drop sequence if exists user_id_seq cascade;
 create sequence user_id_seq;
-drop sequence if exists suggestion_id_seq cascade;
-create sequence suggestion_id_seq;
+--drop sequence if exists suggestion_id_seq cascade;
+--create sequence suggestion_id_seq;
 drop sequence if exists suggest_id_seq cascade;
 create sequence suggest_id_seq;
 drop sequence if exists rate_id_seq cascade;
@@ -17,19 +17,19 @@ create table users(
 	name text,
 	username text unique,
 	password text,
-	division text,
-	department text,
+--	division text,
+--	department text,
 	enc_pass text,
 	salt text
 );
-drop table if exists suggestions cascade;
-create table suggestions(
-	id integer not null primary key default nextval('suggestion_id_seq'),
-	user_id integer references users(id),
-	suggestion text,
-	created_at timestamp,
-	updated_at timestamp
-);
+--drop table if exists suggestions cascade;
+--create table suggestions(
+--	id integer not null primary key default nextval('suggestion_id_seq'),
+--	user_id integer references users(id),
+--	suggestion text,
+--	created_at timestamp,
+--	updated_at timestamp
+--);
 drop table if exists suggests cascade;
 create table suggests(
 	id integer not null primary key default nextval('suggest_id_seq'),
@@ -42,11 +42,11 @@ create table rates(
 	rate text
 );
 -- create view
-drop view if exists user_suggestion_view cascade;
-create view user_suggestion_view as
-	select users.id as userid,users.name,users.department,users.division,suggestions.id as 
-	suggestionid,suggestions.suggestion,suggestions.created_at,suggestions.updated_at from 
-	users join suggestions on users.id=suggestions.user_id;
+--drop view if exists user_suggestion_view cascade;
+--create view user_suggestion_view as
+--	select users.id as userid,users.name,users.department,users.division,suggestions.id as 
+--	suggestionid,suggestions.suggestion,suggestions.created_at,suggestions.updated_at from 
+--	users join suggestions on users.id=suggestions.user_id;
 
 drop view if exists suggest_rate_view cascade;
 create view suggest_rate_view as
